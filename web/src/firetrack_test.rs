2f8cfe9a20d019f529f4f7d4f59e3bcd962c333e
Pieter Frenssen <pieter@frenssen.be>
2020-05-11 22:06:15 +0300

    This is clear now.

--- web/src/firetrack_test.rs
+++ web/src/firetrack_test.rs
@@ -92,16 +92,9 @@ pub fn assert_form_submit(body: &str, label: &str) {
 pub fn get_response_body(response: &HttpResponse) -> String {
     // Get the response body.
     let body = match response.body() {
-        // It is not clear to me under which circumstances this will return a 'Body' or a 'Response'
-        // so let's print out some debugging info.
-        ResponseBody::Body(b) => {
-            println!("'Body' response body is returned.");
-            b
-        }
-        ResponseBody::Other(o) => {
-            println!("'Other' response body is returned.");
-            o
-        }
+        // This returns `Body` for regular route handlers, and `Other` for error handlers.
+        ResponseBody::Body(b) => b,
+        ResponseBody::Other(o) => o,
     };
     // Convert the response in Bytes to a string slice.
     let body = match body {
