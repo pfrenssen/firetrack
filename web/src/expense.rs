512af116e3c54d93177a385007d394c75f0f6568
Pieter Frenssen <pieter@frenssen.be>
2020-08-20 23:50:23 +0300

    Create placeholder for the Expenses page.

---
+++ web/src/expense.rs
@@ -0,0 +1,22 @@
+use super::bootstrap_components::{Alert, AlertType};
+use super::{assert_authenticated, get_tera_context};
+use actix_identity::Identity;
+use actix_session::Session;
+use actix_web::{error, web, Error, HttpResponse};
+use app::AppConfig;
+use db::activation_code::ActivationCodeErrorKind;
+use db::user::UserErrorKind;
+use diesel::PgConnection;
+use validator::validate_email;
+
+// Request handler for the login form.
+pub async fn index_handler(
+    id: Identity,
+    session: Session,
+    tera: web::Data<tera::Tera>,
+) -> Result<HttpResponse, Error> {
+    assert_authenticated(&id)?;
+
+    let content = "Expenses".to_string();
+    Ok(HttpResponse::Ok().content_type("text/html").body(content))
+}
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -11,6 +11,7 @@ use crate::firetrack_test::*;
 
 mod bootstrap_components;
 mod error;
+mod expense;
 mod user;
 
 use actix_identity::{CookieIdentityPolicy, Identity, IdentityService};
@@ -125,6 +126,7 @@ pub fn configure_application(
                         .secure(false),
                 ))
                 .route("/", web::get().to(index))
+                .route("/expenses", web::get().to(expense::index_handler))
                 .route("/favicon.ico", web::get().to(index))
                 .route("/user/activate", web::get().to(user::activate_handler))
                 .route("/user/activate", web::post().to(user::activate_submit))
