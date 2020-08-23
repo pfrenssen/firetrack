ac12b867b9a02e8aeb78ada5c84643786ae388f4
Pieter Frenssen <pieter@frenssen.be>
2020-08-23 17:58:53 +0300

    Provide step definitions for interacting with links in a specific region.

--- features/bootstrap/HtmlContext.php
+++ features/bootstrap/HtmlContext.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use Behat\Mink\Element\NodeElement;
 use Behat\Mink\Exception\UnsupportedDriverActionException;
 use Behat\MinkExtension\Context\RawMinkContext;
 
@@ -10,6 +11,51 @@ use Behat\MinkExtension\Context\RawMinkContext;
  */
 class HtmlContext extends RawMinkContext
 {
+
+    /**
+     * Region CSS selectors keyed by human readable region names.
+     *
+     * @var array
+     */
+    protected $regionMap;
+
+    /**
+     * Constructs a new HtmlContext object.
+     *
+     * @param array $region_map
+     *   An associative array of CSS selectors that identify regions on the
+     *   page, keyed by human readable region names.
+     */
+    public function __construct(array $region_map) {
+        $this->regionMap = $region_map;
+    }
+
+    /**
+     * Returns a region from the current page.
+     *
+     * @param string $region
+     *   The human readable name of the region to return.
+     *
+     * @return \Behat\Mink\Element\NodeElement
+     *   The region.
+     *
+     * @throws \Exception
+     *   If the region cannot be found.
+     */
+    public function getRegion(string $region): NodeElement
+    {
+        $session = $this->getSession();
+        if (!array_key_exists($region, $this->regionMap)) {
+            throw new \Exception(sprintf('Region "%s" is not defined.', $region));
+        }
+
+        $element = $session->getPage()->find('css', $this->regionMap[$region]);
+        if (!$element) {
+            throw new \Exception(sprintf('No region "%s" found on the page %s.', $region, $session->getCurrentUrl()));
+        }
+
+        return $element;
+    }
 
     /**
      * @Then I (should )see the heading :heading
@@ -128,5 +174,73 @@ class HtmlContext extends RawMinkContext
                 )
             );
         }
+    }
+
+    /**
+     * Checks that a link is present in a given region on the page.
+     *
+     * @param string $link
+     *   Link ID, title, text or image alt text.
+     * @param string $region
+     *   The human readable region name.
+     *
+     * @throws \Exception
+     *   If region or link within it cannot be found.
+     *
+     * @Then I should see the link :link in the :region( region)
+     */
+    public function assertLinkRegion(string $link, string $region): void
+    {
+        $region_element = $this->getRegion($region);
+
+        if (empty($region_element->findLink($link))) {
+            throw new \Exception(sprintf('No link to "%s" in the "%s" region on the page %s', $link, $region, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * Checks that a link is present in a given region on the page.
+     *
+     * @param string $link
+     *   Link ID, title, text or image alt text.
+     * @param string $region
+     *   The human readable region name.
+     *
+     * @throws \Exception
+     *   If region or link within it cannot be found.
+     *
+     * @Then I should not see the link :link in the :region( region)
+     */
+    public function assertNotLinkRegion(string $link, string $region): void
+    {
+        $region_element = $this->getRegion($region);
+
+        if (!empty($region_element->findLink($link))) {
+            throw new \Exception(sprintf('Link to "%s" in the "%s" region on the page %s', $link, $region, $this->getSession()->getCurrentUrl()));
+        }
+    }
+
+    /**
+     * Clicks a link in a given region on the page.
+     *
+     * @param string $link
+     *   Link ID, title, text or image alt text.
+     * @param string $region
+     *   The human readable region name.
+     *
+     * @throws \Exception
+     *   If region or link within it cannot be found.
+     *
+     * @When I click :link in the :region( region)
+     */
+    public function assertRegionLinkFollow(string $link, string $region): void
+    {
+        $region_element = $this->getRegion($region);
+
+        $link_element = $region_element->findLink($link);
+        if (empty($link_element)) {
+            throw new \Exception(sprintf('The link "%s" was not found in the region "%s" on the page %s', $link, $region, $this->getSession()->getCurrentUrl()));
+        }
+        $link_element->click();
     }
 }
--- features/user/expenses.feature
+++ features/user/expenses.feature
@@ -5,7 +5,7 @@ Feature: Expenses
 
   Scenario: Add an expense
     Given I am logged in as "pallas.park@email.gr"
-    And I click "Expenses" in the sidebar navigation
+    And I click "Expenses" in the "sidebar navigation"
     Then I should see the heading "Expenses"
     And I should be on "/expenses"
     When I click "Add expense"
