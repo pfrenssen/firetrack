dc26ca1b4ad23ee1e79a7194d840c974d3b3734a
Pieter Frenssen <pieter@frenssen.be>
2021-05-25 20:29:41 +0300

    Switch behat test script to use Selenium.
    This matches what we are using on Travis.

--- scripts/behat.sh
+++ scripts/behat.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# We can use either headless chrome or chromedriver for browser testing.
-# Headless chrome is faster and supported by Travis CI, but cannot be inspected
-# during the test run. Chromedriver can be launched in a window so the test can
-# be followed live. Switch between both using the `javascript_session` option in
-# `behat.yml`.
-
-# Start chromedriver.
-#chromedriver --port=9222 --url-base=wd/hub &
-
-# Start headless chrome.
-chromium --remote-debugging-port=9222 --headless --disable-gpu http://localhost &
-CHROMIUM_PID=$!
+# Start Selenium.
+docker run -d -p 4444:4444 --name=firetrack_selenium --rm --network=host selenium/standalone-chrome:3.11
 
 # Reset database.
-cd db/
+cd db/ || exit 1
 DATABASE_URL=postgres://pieter@localhost/firetrack-test diesel database reset
 cd ..
 
@@ -22,7 +12,7 @@ cd ..
 cargo build
 
 # Start the Mailgun mock server if it is not already running.
-ps aux | grep -v grep | grep mailgun-mock-server &> /dev/null
+pgrep -f mailgun-mock-server &> /dev/null
 if [ $? -eq 1 ]; then
   echo "Mailgun mock server not running. Starting."
   cargo run -- mailgun-mock-server > /dev/null 2>&1 &
@@ -41,5 +31,5 @@ DATABASE_URL=postgres://pieter@localhost/firetrack-test ./vendor/bin/behat -vvv
 # Kill running servers after finishing tests:
 sudo kill -9 $FIRETRACK_SERVER_PID
 
-# Kill chromium.
-sudo kill -9 $CHROMIUM_PID
+# Kill Selenium.
+docker stop firetrack_selenium
