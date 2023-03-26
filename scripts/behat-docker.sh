a2e3bf2429b45279c2aaea4b15e663f1f52ca4d4
Pieter Frenssen <pieter@frenssen.be>
2023-03-26 02:12:20 +0200

    Make docker-composer specific environment variables overridable.

--- scripts/behat-docker.sh
+++ scripts/behat-docker.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-
 set -ex
+
+# Create a .env file with test values.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ ! -f "${SCRIPT_DIR}/../.env" ]; then
+  # Generate comma-separated list of 32 integers in the range of 0-255.
+  SESSION_KEY=$(shuf -i 0-255 -n 32 | tr '\n' ',' | sed 's/,$//')
+
+  # Generate random string of 32 characters for secret key.
+  SECRET_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
+
+  # Write content to file.
+  cat << EOF > .env
+# Some random keys to use in testing.
+SESSION_KEY=$SESSION_KEY
+SECRET_KEY=$SECRET_KEY
+
+RUST_BACKTRACE=1
+
+# Fast, insecure hashing, only good for testing.
+HASHER_MEMORY_SIZE=512
+HASHER_ITERATIONS=2
+EOF
+fi
 
 # Kill any previously running instances.
 docker-compose down
 
 # Prepare containers.
+#docker-compose build firetrack --progress=plain
 docker-compose run composer composer install
 #docker-compose build --progress=plain
 docker-compose run diesel-cli database setup
